867cb2b645b88d97c58a630e6d86a41c6405b271
BillC424 <wcurran44@gmail.com>
2022-07-05 11:48:21 -0400

    Created method to convert string to lowercase so it can be compared with dictionary array. Added sub-method to output number of substrings as a hash

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,1 +1,15 @@
-def substrings
+def substrings(word, dictionary)
+    p lowercase_string = word.downcase
+    
+  
+    
+    sub.reduce(Hash.new(0)) do |result, count|
+      result[count] += 1
+      result
+    end
+  
+  end
+  
+  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  
+  p substrings("Howdy partner, sit down! How's it going?", dictionary)
