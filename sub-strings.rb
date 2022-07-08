c31c05cd58b5cb3840e89cb05ba6377a7de7108d
BillC424 <wcurran44@gmail.com>
2022-07-08 00:06:36 -0400

    Added #split method to first variable so the string argument could be split up into words and iterated over for each dictionary word using the select enumerable nested within the reduce enumerable. Also added delete_if method to exclude all dictionary words that did not appear in the string argument

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,13 +1,12 @@
 def substrings(word, dictionary)
-    p lowercase_string = word.downcase
-    
-  
-    
-    sub.reduce(Hash.new(0)) do |result, count|
-      result[count] += 1
-      result
+    string_array = word.downcase.split
+    substring_list = dictionary.reduce(Hash.new(0)) do |result, substring|
+      sub_freq = string_array.select {|sw| sw.include?(substring)}
+      sub_freq_total = sub_freq.count
+      result[substring] += sub_freq_total
+      result      
     end
-  
+    substring_list.delete_if {|key, value| value == 0}
   end
   
   dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
